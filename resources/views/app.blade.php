25ad1e2f42654bfc18854fd84e256bbf89e21637
catris25 <blue.star95@outlook.com>
2015-11-05 22:10:41 +0700

    Revert "Fix the bootstrap cdn link"
    This reverts commit 86513d946137335071011813ec91cc8295207a9c.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,7 @@
 
 <html lang="en">
 <head>
-	<!-- Scripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -61,5 +59,8 @@
 
 	@yield('content')
 
+	<!-- Scripts -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </body>
 </html>
