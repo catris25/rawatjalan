dc64c28ff130cf5667e691a17677569bec5de7ee
catris25 <blue.star95@outlook.com>
2015-11-05 22:10:30 +0700

    Attempting to create multiple login
    have created db migration, controller, middleware, and decided it's not
    what I wanted.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,12 +45,14 @@
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
+						<!-- <li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<ul class="dropdown-menu"> -->
+								<li>
+									<a href="{{ url('/auth/logout') }}">Logout</a>
+								</li>
+							<!-- </ul>
+						</li> -->
 					@endif
 				</ul>
 			</div>
