baaff20729db66100cacf734dde2b0979637cd0a
catris25 <blue.star95@outlook.com>
2015-11-05 22:19:12 +0700

    a small change in interface

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,12 +45,12 @@
 						<li><a href="{{ url('/auth/login') }}">Login</a></li>
 						<li><a href="{{ url('/auth/register') }}">Register</a></li>
 					@else
-						<li class="dropdown">
+						<!-- <li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
+							<ul class="dropdown-menu" role="menu"> -->
 								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
+							<!-- </ul>
+						</li> -->
 					@endif
 				</ul>
 			</div>
