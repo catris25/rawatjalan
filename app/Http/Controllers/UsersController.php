f2e2fcd81e7ab86b21872984ad09e79159572609
catris25 <blue.star95@outlook.com>
2015-11-16 15:08:40 +0700

    Fix Register

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 use App\User;
 use App\Admin;
 use App\Dokter;
+use App\Http\Controllers\Controller;
 use Input;
 use Bican\Roles\Exceptions\PermissionDeniedException;
 use Bican\Roles\Models\Permission;
 use Bican\Roles\Models\Role;
 use Bican\Roles\Traits\HasRoleAndPermission;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -30,7 +32,7 @@ class UsersController extends Controller{
     public function postAdminRegister(NewUserRequest $request, User $users, Admin $admin) {
 
 
-        $new_users = $admin->create([
+        $new_users = Admin::create([
             'nama_admin' => $request->input('nama_admin'),
             'NIK' => $request->input('NIK'),
             'alamat' => $request->input('alamat'),
@@ -39,19 +41,18 @@ class UsersController extends Controller{
             'email' => Str::lower($request->input('email')),
         ]);
 
-        $new_users = $users->create([
+        $new_users = User::create([
             'email' => Str::lower($request->input('email')),
             'password' => bcrypt($request->input('password')),
         ]);
 
+        $lastInsertedID = $new_users->email;
+        $userID = DB::table('users')->where('email', $lastInsertedID)->value('id');
+        var_dump($userID);
         if($new_users){
-            //$userID = Input::get('email');
-            $userID = $users->getId();
-            $new_users = User::find($userID);
-            $role = Role::find('RL010');
-            //$new_users = User::find('id');
-            $new_users->attachRole($role);
-            //flash()->success('User Added Successfully!');
+          $new_users = User::find($userID);
+          $role = Role::find('RL001');
+          $new_users->attachRole($role);
         } else {
             //flash()->error('An error occurred, try adding the User again!');
         }
