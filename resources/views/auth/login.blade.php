6c4ac73323c7fff4938b42ebc98a5b918baec32f
12blackpearls <12blackpearls@gmail.com>
2015-11-19 23:31:58 +0700

    Change to materialize.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,12 @@
-@extends('app')
+@extends('master')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid" style="margin-top:1%;">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default" style="background-color:#FBFBFB;padding-top:3%;padding-bottom:3%;">
 				<div class="panel-heading">Login</div>
-				<div class="panel-body">
+				<div class="panel-body" style="background-color:#FBFBFB;margin: 0 auto;">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
@@ -21,21 +21,21 @@
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+						<div class="row">
+							<div class="input-field col s12">
+								<input id="email" type="email" class="validate" name="email" value="{{ old('email') }}">
+								<label for="email">E-Mail Address</label>
 							</div>
 						</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+						<div class="row">
+							<div class="input-field col s12">
+								<input id="password" type="password" class="validate" name="password">
+								<label for="password">Password</label>
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="row">
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,28 +1,63 @@
 <html>
 <head>
     <title> @yield('title') </title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">-->
     <!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
     <link href="/rawatjalan/public/css/roboto.min.css" rel="stylesheet">
-    <link href="/rawatjalan/public/css/material.min.css" rel="stylesheet">
+    <link href="/rawatjalan/public/css/bootstrap.css" rel="stylesheet">
+    <!--<link href="/rawatjalan/public/css/material.min.css" rel="stylesheet">-->
     <link href="/rawatjalan/public/css/ripples.min.css" rel="stylesheet">
+    <link href="/rawatjalan/public/css/materialize.min.css" rel="stylesheet">
 
 </head>
+<style>
+    a:hover {
+      text-decoration: none;
+      color:#FFFFFF;
+    }
+</style>
+
 <body>
 
-@include('navbar')
+@include('navbars')
 
 @yield('content')
 
-<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<!--<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>-->
 
 <script src="/rawatjalan/public/js/ripples.min.js"></script>
-<script src="/rawatjalan/public/js/material.min.js"></script>
-<script>
+<!--<script src="/rawatjalan/public/js/material.min.js"></script>-->
+<script src="/rawatjalan/public/js/materialize.js"></script>
+<!-- <script>
     $(document).ready(function() {
         // This command is used to initialize some elements and make them work properly
         $.material.init();
+    });
+</script> -->
+<script>
+    $(document).ready(function() {
+    	$('select').material_select();
+    	$(".button-collapse").sideNav();
+    });
+</script>
+	
+<script>
+	$(document).ready(function() {
+    	$(".dropdown-button").dropdown({
+    		constrain-width: true;
+    		hover: true;
+    		belowOrigin: true;
+    	});
+    	
+    });
+</script>
+
+<script>
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 </script>
 </body>
