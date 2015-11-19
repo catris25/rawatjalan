c9b6efad7e78c6e452b0e7c2b0273e376c1a99dc
12blackpearls <12blackpearls@gmail.com>
2015-11-19 23:41:13 +0700

    Delete //HEAD Insertion

--- resources/views/pasien/tambah.blade.php
+++ resources/views/pasien/tambah.blade.php
@@ -13,15 +13,15 @@
        @endforeach
      </ul>
 
-<<<<<<< HEAD
+
 
        <div class="col-md-8 col-md-offset-2">
 
          <form class="form s10" role="form" method="POST" action="{{ url('pasien/tambah') }}">
-=======
-       <div class="col-md-6">
+
+       <div class="col-md-12">
          <form class="form-horizontal" role="form" method="POST" action="{{ url('pasien/tambah') }}">
->>>>>>> refs/remotes/catris25/master
+
   						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 
