cfb54287ae35af9b59bf545bb3a536979a4e9b3d
catris25 <blue.star95@outlook.com>
2015-12-12 17:44:41 +0700

    Fix migration
    Migrations are fixed, all foreign key constraints are now working
    ADTableSeeder is still not working

--- database/migrations/2015_12_12_094326_create_dokter_table.php
+++ database/migrations/2015_12_12_094326_create_dokter_table.php
@@ -15,12 +15,16 @@ class CreateDokterTable extends Migration
             $table->string('alamat',75);
             $table->string('telepon',12);
             $table->date('tanggal_lahir');
-            $table->string('id_poli', 5)->foreign('id_poli')->references('id')->on('poli');
+            $table->string('id_poli', 5);
             $table->string('spesialisasi',30);
             $table->string('email')->unique();
             $table->rememberToken();
             $table->timestamps();
 
+        });
+
+        Schema::table('dokter', function(Blueprint $table) {
+            $table->foreign('id_poli')->references('id')->on('poli');
         });
 
     }
