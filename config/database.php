567fdedea9932ae940adeb6e4c034a0b7f51916a
12blackpearls <12blackpearls@gmail.com>
2015-12-18 12:50:13 +0700

    Fix Postgre

--- config/database.php
+++ config/database.php
@@ -61,10 +61,10 @@ return [
 
         'mysql' => [
             'driver'    => 'pgsql',
-            'host'      => $host,
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'rawatjalan'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
@@ -73,10 +73,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
@@ -124,7 +124,7 @@ return [
 
         'default' => [
             'host'     => '127.0.0.1',
-            'port'     => 5432,
+            'port'     => 6379,
             'database' => 0,
         ],
 
